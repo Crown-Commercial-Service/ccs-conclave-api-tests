c4aab048d4479b14eedea2129c14cdeb718e1513
himaninare <73709815+himaninare@users.noreply.github.com>
2020-12-02 20:45:52 +0000

    Added testng extent reports and logging

--- src/test/java/com/ccs/conclave/cii/api/tests/GetSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/cii/api/tests/GetSchemeInfoTests.java
@@ -1,4 +1,15 @@
 package com.ccs.conclave.cii.api.tests;
 
-public class GetSchemeInfoTests {
+import com.ccs.comclave.cii.api.listeners.BaseClass;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GetSchemeInfoTests extends BaseClass {
+
+    @Test
+    public void myFirstTest(){
+         String response = "Hello";
+        Reporter.log("Response is: " + response, true);
+    }
+
 }
