b76c2fbf88fbbbb90f9dc45a05b995b0baee8cf4
amrutha sireesh <amrutha.sireesh@co-cc02846.ad.cabinetoffice.gov.uk>
2020-12-03 08:21:53 +0000

    Add GetSchemeInfo tests

--- src/test/java/com/ccs/conclave/cii/api/tests/GetSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/cii/api/tests/GetSchemeInfoTests.java
@@ -1,4 +1,11 @@
 package com.ccs.conclave.cii.api.tests;
 
+import org.testng.annotations.Test;
+
 public class GetSchemeInfoTests {
+
+    @Test
+    public void testGetCompaniesHouseSchemeInfo() {
+
+    }
 }
