df463215950e926c7b2c3e9362f205204cc67541
himaninare <73709815+himaninare@users.noreply.github.com>
2020-12-15 17:27:35 +0000

    Added logging and travis file

--- src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
@@ -4,14 +4,18 @@ import com.ccs.conclave.api.cii.data.OrgDataProvider;
 import com.ccs.conclave.api.cii.pojo.SchemeInfo;
 import com.ccs.conclave.api.cii.requests.RestRequests;
 import com.ccs.conclave.api.cii.response.SchemeInfoResponse;
+import com.ccs.conclave.api.common.BaseClass;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.response.Response;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 import static com.ccs.conclave.api.cii.verification.VerifyResponses.*;
 import static com.ccs.conclave.api.common.SchemeRegistry.*;
 
-public class GetSchemeInfoTests {
+public class GetSchemeInfoTests extends BaseClass {
+
+    private final static Logger logger = Logger.getLogger(GetSchemeInfoTests.class);
 
     @Test
     public void getCompaniesHouseSchemeInfo() {
@@ -19,6 +23,7 @@ public class GetSchemeInfoTests {
         SchemeInfo schemeInfo = orgData.getInfo(COMPANIES_HOUSE);
         Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
         verifyGetSchemeInfoResponse(orgData, response.as(SchemeInfoResponse.class));
+        logger.info("Response is"+ response.asString());
     }
 
     @Test
