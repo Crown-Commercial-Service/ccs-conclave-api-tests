40e710aaf5994bf904356b726423619ff966e6e3
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2020-12-22 11:48:20 +0000

    Update response verification

--- src/main/java/com/ccs/conclave/api/cii/verification/VerifyResponses.java
+++ src/main/java/com/ccs/conclave/api/cii/verification/VerifyResponses.java
@@ -4,6 +4,7 @@ import com.ccs.conclave.api.cii.data.OrgDataProvider;
 import com.ccs.conclave.api.cii.pojo.SchemeName;
 import com.ccs.conclave.api.cii.response.SchemeInfoResponse;
 import com.ccs.conclave.api.cii.response.SchemeNamesResponse;
+import com.ccs.conclave.api.common.SchemeRegistry;
 import io.restassured.response.Response;
 import org.testng.Assert;
 
@@ -11,8 +12,8 @@ import static com.ccs.conclave.api.common.SchemeRegistry.*;
 
 public class VerifyResponses {
 
-    public static void verifyGetSchemeInfoResponse(OrgDataProvider actualRes, SchemeInfoResponse expectedRes) {
-        Assert.assertEquals(actualRes, expectedRes, "GetSchemeInfoResponse is not matching!!");
+    public static void verifyGetSchemeInfoResponse(SchemeRegistry registry, OrgDataProvider expectedRes, SchemeInfoResponse actualRes) {
+        Assert.assertEquals(actualRes.getSchemeInfo(), expectedRes.getInfo(registry), "Wrong GetSchemeInfo response!" );
     }
 
     public static void verifyGetSchemeNamesResponse(Response response) {
@@ -23,30 +24,28 @@ public class VerifyResponses {
         Assert.assertEquals(schemeNameCOH.getSchemeURI(), getSchemeURL(COMPANIES_HOUSE), "Wrong SchemeURL!");
         Assert.assertEquals(schemeNameCOH.getSchemeCountryCode(), getSchemeCountryCode(COMPANIES_HOUSE), "Wrong CountryCode!");
 
-        SchemeName schemeNameDUNS = schemeNamesResponse.getSchemeCOH();
+        SchemeName schemeNameDUNS = schemeNamesResponse.getSchemeDUNS();
         Assert.assertEquals(schemeNameDUNS.getSchemeRegisterCode(), getSchemeCode(DUNS_AND_BRADSTREET), "Wrong SchemeCode!");
         Assert.assertEquals(schemeNameDUNS.getSchemeName(), getSchemeName(DUNS_AND_BRADSTREET), "Wrong SchemeName!");
         Assert.assertEquals(schemeNameDUNS.getSchemeURI(), getSchemeURL(DUNS_AND_BRADSTREET), "Wrong SchemeURL!");
         Assert.assertEquals(schemeNameDUNS.getSchemeCountryCode(), getSchemeCountryCode(DUNS_AND_BRADSTREET), "Wrong CountryCode!");
 
-        SchemeName schemeNameCHC = schemeNamesResponse.getSchemeCOH();
+        SchemeName schemeNameCHC = schemeNamesResponse.getSchemeCHC();
         Assert.assertEquals(schemeNameCHC.getSchemeRegisterCode(), getSchemeCode(CHARITIES_COMMISSION), "Wrong SchemeCode!");
         Assert.assertEquals(schemeNameCHC.getSchemeName(), getSchemeName(CHARITIES_COMMISSION), "Wrong SchemeName!");
         Assert.assertEquals(schemeNameCHC.getSchemeURI(), getSchemeURL(CHARITIES_COMMISSION), "Wrong SchemeURL!");
         Assert.assertEquals(schemeNameCHC.getSchemeCountryCode(), getSchemeCountryCode(CHARITIES_COMMISSION), "Wrong CountryCode!");
 
-        SchemeName schemeNameNIC = schemeNamesResponse.getSchemeCOH();
+        SchemeName schemeNameNIC = schemeNamesResponse.getSchemeNIC();
         Assert.assertEquals(schemeNameNIC.getSchemeRegisterCode(), getSchemeCode(NORTHERN_ISLAND_CHARITY), "Wrong SchemeCode!");
         Assert.assertEquals(schemeNameNIC.getSchemeName(), getSchemeName(NORTHERN_ISLAND_CHARITY), "Wrong SchemeName!");
         Assert.assertEquals(schemeNameNIC.getSchemeURI(), getSchemeURL(NORTHERN_ISLAND_CHARITY), "Wrong SchemeURL!");
         Assert.assertEquals(schemeNameNIC.getSchemeCountryCode(), getSchemeCountryCode(NORTHERN_ISLAND_CHARITY), "Wrong CountryCode!");
 
-        SchemeName schemeNameSC = schemeNamesResponse.getSchemeCOH();
+        SchemeName schemeNameSC = schemeNamesResponse.getSchemeSC();
         Assert.assertEquals(schemeNameSC.getSchemeRegisterCode(), getSchemeCode(SCOTLAND_CHARITY), "Wrong SchemeCode!");
         Assert.assertEquals(schemeNameSC.getSchemeName(), getSchemeName(SCOTLAND_CHARITY), "Wrong SchemeName!");
         Assert.assertEquals(schemeNameSC.getSchemeURI(), getSchemeURL(SCOTLAND_CHARITY), "Wrong SchemeURL!");
         Assert.assertEquals(schemeNameSC.getSchemeCountryCode(), getSchemeCountryCode(SCOTLAND_CHARITY), "Wrong CountryCode!");
-
-
     }
 }
--- src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
@@ -6,7 +6,6 @@ import com.ccs.conclave.api.cii.requests.RestRequests;
 import com.ccs.conclave.api.cii.response.SchemeInfoResponse;
 import com.ccs.conclave.api.common.BaseClass;
 import io.restassured.response.Response;
-import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 import static com.ccs.conclave.api.cii.verification.VerifyResponses.*;
@@ -14,15 +13,12 @@ import static com.ccs.conclave.api.common.SchemeRegistry.*;
 
 public class GetSchemeInfoTests extends BaseClass {
 
-    private final static Logger logger = Logger.getLogger(GetSchemeInfoTests.class);
-
     @Test
     public void getCompaniesHouseSchemeInfo() {
         OrgDataProvider orgData = new OrgDataProvider();
         SchemeInfo schemeInfo = orgData.getInfo(COMPANIES_HOUSE);
         Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(orgData, response.as(SchemeInfoResponse.class));
-        logger.info("Response is"+ response.asString());
+        verifyGetSchemeInfoResponse(COMPANIES_HOUSE, orgData, response.as(SchemeInfoResponse.class));
     }
 
     @Test
