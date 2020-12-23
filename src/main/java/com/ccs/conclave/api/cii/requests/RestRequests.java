8f30588f0e5eb6a476e05590d06eddc233c419fa
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2020-12-23 13:22:15 +0000

    setup environment variable

--- src/main/java/com/ccs/conclave/api/cii/requests/RestRequests.java
+++ src/main/java/com/ccs/conclave/api/cii/requests/RestRequests.java
@@ -4,24 +4,30 @@ import com.ccs.conclave.api.common.Endpoints;
 import com.ccs.conclave.api.common.SchemeRegistry;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
+import org.apache.log4j.Logger;
 
 import static com.ccs.conclave.api.common.SchemeRegistry.*;
 import static io.restassured.RestAssured.given;
 
 public class RestRequests {
+    private final static Logger logger = Logger.getLogger(RestRequests.class);
+    private static String baseURI = System.getProperty("base.url");
+
     public static Response getSchemeInfo(SchemeRegistry scheme, String identifier) {
-        String baseURI = Endpoints.getSchemeInfoURI + getSchemeCode(scheme) + "/" + identifier;
-        return get(baseURI);
+        logger.info("getSchemeInfo Endpoint: "+ baseURI);
+        String endpoint = baseURI + Endpoints.getSchemeInfoURI + getSchemeCode(scheme) + "/" + identifier;
+        logger.info("getSchemeInfo Endpoint: "+ endpoint);
+        return get(endpoint);
     }
 
     public static Response getSchemeNames() {
-        String baseURI = Endpoints.getSchemeNamesURI;
-        return get(baseURI);
+        String endpoint = baseURI + Endpoints.getSchemeNamesURI;
+        return get(endpoint);
     }
 
     public static Response postSchemeInfo(SchemeRegistry scheme, String identifier) {
-        String baseURI = Endpoints.postSchemeInfo + getSchemeCode(scheme) + "/" + identifier;
-        return post(baseURI);
+        String endpoint = baseURI + Endpoints.postSchemeInfo + getSchemeCode(scheme) + "/" + identifier;
+        return post(endpoint);
     }
 
     private static Response get(String baseURI) {
--- src/main/java/com/ccs/conclave/api/common/Endpoints.java
+++ src/main/java/com/ccs/conclave/api/common/Endpoints.java
@@ -1,7 +1,7 @@
 package com.ccs.conclave.api.common;
 
 public class Endpoints {
-    public static String getSchemeNamesURI = "";
-    public static String getSchemeInfoURI = "";
+    public static String getSchemeNamesURI = "/identities/schemes/";
+    public static String getSchemeInfoURI = "/identities/schemes/organisations/";
     public static String postSchemeInfo = "";
 }
