90ba2bc8513d3a800a25179418901b21f24655a1
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2021-02-27 08:58:35 +0000

    Rename endpoints

--- src/main/java/com/ccs/conclave/api/common/Endpoints.java
+++ src/main/java/com/ccs/conclave/api/common/Endpoints.java
@@ -3,9 +3,9 @@ package com.ccs.conclave.api.common;
 public class Endpoints {
     public static String getSchemesURI = "/identities/schemes";
     public static String getSchemeInfoURI = "/identities/schemes/organisation?";
-    public static String postSchemeInfo = "/identities/schemes/organisation";
-    public static String updateScheme = "/identities/schemes/organisation";
-    public static String deleteScheme = "/identities/schemes/organisation";
-    public static String deleteOrganisation = "/identities/organisation?";
+    public static String postSchemeInfoURI = "/identities/schemes/organisation";
+    public static String updateSchemeURI = "/identities/schemes/organisation";
+    public static String deleteSchemeURI = "/identities/schemes/organisation";
+    public static String deleteOrganisationURI = "/identities/organisation?";
     public static String adminGetSchemeInfoURI = "/identities/schemes/manageidentifiers?";
 }
