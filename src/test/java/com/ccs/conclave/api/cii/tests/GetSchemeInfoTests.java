d5b34b2c4e9dcfb0e3ea7b93102ea05978407b86
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2021-03-18 13:24:35 +0000

    modify Get and Post tests for Sf changes

--- src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/api/cii/tests/GetSchemeInfoTests.java
@@ -14,86 +14,86 @@ public class GetSchemeInfoTests extends BaseClass {
 
     @Test
     public void getCompaniesHouseSchemeInfo() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
-        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
+        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test //Bug: CON-470 FIXED
     public void getDunsSchemeInfoWithoutAdditionalIdentifier() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET);
-        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET);
+        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test
     public void getDunsSchemeInfoWithCOH() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_COH);
-        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_COH);
+        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_COH, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test //Bug: CON-470 -FIXED
     public void getDunsSchemeInfoWithCOHAndCHC() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_CHC_AND_COH);
-        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_CHC_AND_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_CHC_AND_COH);
+        Response response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_CHC_AND_COH, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-450 -FIXED
     public void getGBCharitiesSchemeInfo() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION);
-        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION);
+        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
 //    @Test // Bug: CON-452 -FIXED
 //    public void getGBCharitiesSchemeInfoWithTwoCOH() {
-//        SchemeInfo schemeInfo = OrgDataProvider.getInfo(CHARITIES_COMMISSION_WITH_TWO_COH);
-//        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_TWO_COH, schemeInfo.getIdentifier().getId());
-//        verifyGetSchemeInfoResponse(schemeInfo, response);
+//        SchemeInfo expSchemeInfo = OrgDataProvider.getInfo(CHARITIES_COMMISSION_WITH_TWO_COH);
+//        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_TWO_COH, expSchemeInfo.getIdentifier().getId());
+//        verifyGetSchemeInfoResponse(expSchemeInfo, response);
 //    }
 
     @Test // Bug: CON-452 -FIXED
     public void getGBCharitiesSchemeInfoWithCOHAndSC() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION_WITH_COH_AND_SC);
-        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_COH_AND_SC, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION_WITH_COH_AND_SC);
+        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_COH_AND_SC, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-452 -FIXED
     public void getGBCharitiesSchemeInfoWithKnownAndUnknownIdentifiers() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS);
-        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(CHARITIES_COMMISSION_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS);
+        Response response = RestRequests.getSchemeInfo(CHARITIES_COMMISSION_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-489 -FIXED
     public void getNorthernCharitySchemeInfo() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(NORTHERN_CHARITY);
-        Response response = RestRequests.getSchemeInfo(NORTHERN_CHARITY, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(NORTHERN_CHARITY);
+        Response response = RestRequests.getSchemeInfo(NORTHERN_CHARITY, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-439
     public void getNorthernCharitySchemeInfoWithCOH() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(NORTHERN_CHARITY_WITH_COH);
-        Response response = RestRequests.getSchemeInfo(NORTHERN_CHARITY_WITH_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(NORTHERN_CHARITY_WITH_COH);
+        Response response = RestRequests.getSchemeInfo(NORTHERN_CHARITY_WITH_COH, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-489 -FIXED
     public void getScottishCharitySchemeInfo() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY);
-        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY);
+        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test // Bug: CON-452 -FIXED
     public void getScottishCharitySchemeInfoWithCOHAndCHC() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY_WITH_CHC_COH);
-        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY_WITH_CHC_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY_WITH_CHC_COH);
+        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY_WITH_CHC_COH, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test
@@ -110,34 +110,34 @@ public class GetSchemeInfoTests extends BaseClass {
     @Test // Bug: CON-533 - FIXED
     //Note : Duplicate check for already registered additional identifier
     public void getDuplicateWithAdditionalIdentifier() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_COH);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(DUN_AND_BRADSTREET_WITH_COH);
 
-        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getAdditionalIdentifiers().get(0).getId());
+        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, expSchemeInfo.getAdditionalIdentifiers().get(0).getId());
         RestRequests.postSchemeInfo(response.asString());
 
-        response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_COH, schemeInfo.getIdentifier().getId());
+        response = RestRequests.getSchemeInfo(DUN_AND_BRADSTREET_WITH_COH, expSchemeInfo.getIdentifier().getId());
         verifyResponseCodeForDuplicateResource(response);
     }
 
     @Test // Bug: CON-662 - Fixed
     public void getSchemeInfoWithAlreadyRegIDAndInvalidScheme() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
 
-        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
+        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, expSchemeInfo.getIdentifier().getId());
         RestRequests.postSchemeInfo(response.asString());
 
-        response = RestRequests.getSchemeInfo(NORTHERN_CHARITY, schemeInfo.getIdentifier().getId());
+        response = RestRequests.getSchemeInfo(NORTHERN_CHARITY, expSchemeInfo.getIdentifier().getId());
         verifyInvalidIdResponse(response);
     }
 
     @Test // Bug: CON-662 - Fixed
     public void getSchemeInfoWithSpaceInSchemeID() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(COMPANIES_HOUSE);
 
-        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
+        Response response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, expSchemeInfo.getIdentifier().getId());
         RestRequests.postSchemeInfo(response.asString());
 
-        String IdWithSpace = schemeInfo.getIdentifier().getId() + " ";
+        String IdWithSpace = expSchemeInfo.getIdentifier().getId() + " ";
         response = RestRequests.getSchemeInfo(COMPANIES_HOUSE, IdWithSpace);
         verifyResponseCodeForSuccess(response);
     }
@@ -156,9 +156,9 @@ public class GetSchemeInfoTests extends BaseClass {
 
     @Test
     public void getActiveSCCharitiesWithInactiveAndActiveAdditionalIDInfo() {
-        SchemeInfo schemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS);
-        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, response);
+        SchemeInfo expSchemeInfo = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS);
+        Response response = RestRequests.getSchemeInfo(SCOTLAND_CHARITY_WITH_KNOWN_AND_UNKNOWN_IDENTIFIERS, expSchemeInfo.getIdentifier().getId());
+        verifyGetSchemeInfoResponse(expSchemeInfo, response);
     }
 
     @Test
--- src/test/java/com/ccs/conclave/api/cii/tests/PostSchemeInfoTests.java
+++ src/test/java/com/ccs/conclave/api/cii/tests/PostSchemeInfoTests.java
@@ -1,6 +1,5 @@
 package com.ccs.conclave.api.cii.tests;
 
-import com.ccs.conclave.api.cii.data.OrgDataProvider;
 import com.ccs.conclave.api.cii.pojo.SchemeInfo;
 import com.ccs.conclave.api.cii.requests.RestRequests;
 import com.ccs.conclave.api.common.BaseClass;
@@ -8,7 +7,7 @@ import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
-import static com.ccs.conclave.api.cii.data.OrgDataProvider.getExpSchemeInfoWithoutAddIdentifiers;
+import static com.ccs.conclave.api.cii.data.OrgDataProvider.*;
 import static com.ccs.conclave.api.cii.data.RequestPayloads.*;
 import static com.ccs.conclave.api.cii.data.SchemeRegistry.*;
 import static com.ccs.conclave.api.cii.requests.RestRequests.deleteOrganisation;
@@ -21,15 +20,15 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoCOH_NoAddIdentifier() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(COMPANIES_HOUSE);
+        SchemeInfo expectedSchemeInfo = getExpectedSchemeInfo(COMPANIES_HOUSE);
 
         // Perform Get call to form the request payload for POST call
-        Response getSchemeRes = getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        Response getSchemeRes = getSchemeInfo(COMPANIES_HOUSE, expectedSchemeInfo.getIdentifier().getId());
+        verifyResponseCodeForSuccess(getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
-        verifyPostSchemeInfoResponse(schemeInfo, postSchemeRes);
+        verifyPostSchemeInfoResponse(expectedSchemeInfo, postSchemeRes);
 
         logger.info("verifying duplicate organisation registration.....");
 
@@ -40,7 +39,7 @@ public class PostSchemeInfoTests extends BaseClass {
         logger.info("verifying duplicate organisation registration in Get Scheme info.....");
 
         // verify duplicate check for Get call
-        getSchemeRes = getSchemeInfo(COMPANIES_HOUSE, schemeInfo.getIdentifier().getId());
+        getSchemeRes = getSchemeInfo(COMPANIES_HOUSE, expectedSchemeInfo.getIdentifier().getId());
         verifyResponseCodeForDuplicateResource(getSchemeRes);
 
         logger.info("deleting org. identifier after test..");
@@ -51,11 +50,11 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoDUNS_With_MultipleAddIdentifiers() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(DUN_AND_BRADSTREET_WITH_CHC_AND_COH);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(DUN_AND_BRADSTREET_WITH_CHC_AND_COH);
 
         // Perform Get call to form the request payload for POST call
         Response getSchemeRes = getSchemeInfo(DUN_AND_BRADSTREET_WITH_CHC_AND_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        verifyResponseCodeForSuccess(getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
@@ -83,11 +82,11 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoCHC_With_MultipleAddIdentifiers() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(CHARITIES_COMMISSION_WITH_COH_AND_SC);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(CHARITIES_COMMISSION_WITH_COH_AND_SC);
 
         // Perform Get call to form the request payload for POST call
         Response getSchemeRes = getSchemeInfo(CHARITIES_COMMISSION_WITH_COH_AND_SC, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        verifyResponseCodeForSuccess(getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
@@ -107,11 +106,11 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoSC_With_NoAddIdentifiers() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(SCOTLAND_CHARITY);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(SCOTLAND_CHARITY);
 
         // Perform Get call to form the request payload for POST call
         Response getSchemeRes = getSchemeInfo(SCOTLAND_CHARITY, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        verifyResponseCodeForSuccess(getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
@@ -131,11 +130,12 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoNIC_With_OneAddIdentifiers() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(NORTHERN_CHARITY_WITH_COH);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(NORTHERN_CHARITY_WITH_COH);
+        SchemeInfo schemeInfoWithoutSF = getExpSchemeInfoWithoutSFIdentifier(NORTHERN_CHARITY_WITH_COH);
 
         // Perform Get call to form the request payload for POST call
         Response getSchemeRes = getSchemeInfo(NORTHERN_CHARITY_WITH_COH, schemeInfo.getIdentifier().getId());
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        verifyGetSchemeInfoResponse(schemeInfoWithoutSF, getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
@@ -155,13 +155,14 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoSC_Without_Prefix() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(SCOTLAND_CHARITY);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(SCOTLAND_CHARITY);
+        SchemeInfo schemeInfoWithoutSF = getExpSchemeInfoWithoutSFIdentifier(SCOTLAND_CHARITY);
 
         // Perform Get call to form the request payload for POST call
         String IdWithPrefix = schemeInfo.getIdentifier().getId();
         String IdWithoutPrefix = IdWithPrefix.replaceAll("[^0-9]", "");
         Response getSchemeRes = getSchemeInfo(SCOTLAND_CHARITY, IdWithoutPrefix);
-        verifyGetSchemeInfoResponse(schemeInfo, getSchemeRes); // verify Get SchemeInfo response before passing to Post
+        verifyGetSchemeInfoResponse(schemeInfoWithoutSF, getSchemeRes);
 
         // Perform Post Operation
         Response postSchemeRes = RestRequests.postSchemeInfo(getSchemeRes.asString());
@@ -181,7 +182,7 @@ public class PostSchemeInfoTests extends BaseClass {
 
     @Test
     public void postSchemeInfoNIC_Without_Prefix() {
-        SchemeInfo schemeInfo = OrgDataProvider.getExpectedSchemeInfo(NORTHERN_CHARITY_WITH_COH);
+        SchemeInfo schemeInfo = getExpectedSchemeInfo(NORTHERN_CHARITY_WITH_COH);
 
         // Perform Get call to form the request payload for POST call
         String IdWithPrefix = schemeInfo.getIdentifier().getId();
